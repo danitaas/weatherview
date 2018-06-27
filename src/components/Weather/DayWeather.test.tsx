a563fd9ea2439147a585a735e27e30a03ea5a749
code <code@polectic.com>
2018-06-27 01:34:56 +0100

    snapshot tests

--- src/components/Weather/DayWeather.test.tsx
+++ src/components/Weather/DayWeather.test.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import DayWeather from './DayWeather';
+// todo: cant use typescript import without correcting toolchain
+const ReactShallowRenderer = require("react-test-renderer/shallow");
+const ReactTestRenderer = require("react-test-renderer");
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -12,4 +15,16 @@ it('renders all props', () => {
     const div = document.createElement('div');
     ReactDOM.render(<DayWeather day={"a"} mintemp={"a"} maxtemp={"a"} conditions={"a"} wind={"a"} />, div);
     ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders shallow", () => {
+    const renderer = new ReactShallowRenderer();
+    renderer.render(<DayWeather />);
+    const result = renderer.getRenderOutput();
+    expect(result.type).toBe("div");
+});
+
+it("renders", () => {
+    const tree = ReactTestRenderer.create(<DayWeather />).toJSON();
+    expect(tree).toMatchSnapshot();
 });
